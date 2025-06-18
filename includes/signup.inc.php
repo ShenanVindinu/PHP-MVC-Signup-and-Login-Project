9444fda3aaa1ed0d47e92fc4b7ff7b1a7cd564f5
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-18 13:24:04 +0530

    Signup Error Handlers added

---
+++ includes/signup.inc.php
@@ -0,0 +1,52 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    // Get form data from POST request
+    $username  =  $_POST["username"];
+    $pwd       =  $_POST["pwd"];
+    $email     =  $_POST["email"];
+
+    try {
+        
+        require_once 'dbh.inc.php';
+        require_once 'signup_model.inc.php';
+        require_once 'signup_contr.inc.php';
+
+        // ERROR HANDLERS
+        $errors = [];
+
+
+        if (is_input_empty($username, $pwd, $email)) {
+            $errors["empty_input"] = "Fill in all fields!";
+        }    
+
+        if (is_email_invalid($email)) {
+            $errors["invalid email"] = "Invalid email used!";
+        }
+        
+        if (is_username_taken($pdo, $username)) {
+            $errors["username_taken"] = "Username already taken!";
+        }
+
+        if (is_email_registered($pdo, $email)) {
+            $errors["email_used"] = "Email Already Registered!";
+        }
+
+        require_once 'config_session.inc.php';
+
+        if ($errors) {
+            $_SESSION["errors_signup"] = $errors;
+            header("Location: ../index.php");
+        }
+
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+
+} else {
+    // If the request is not a POST (e.g., direct access), redirect to index.php
+    header("Location: ../index.php");
+    die();
+}
