143abf00478996694822dfc121d8ec591a8e4208
Shenan Vindinu <shenanvindinu@gmail.com>
2025-06-18 14:52:32 +0530

    Index updated to handle signup inputs

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/main.css">
   <title>Document</title>
-
 </head>
 
 <body>
@@ -27,9 +26,9 @@
 
     <h3>Signup</h3>
         <form action="includes/signup.inc.php" method="post">
-            <input type="text" name="username" placeholder="Username">
-            <input type="password" name="pwd" placeholder="Password">
-            <input type="text" name="email" placeholder="E-Mail">
+            <?php
+                signup_inputs();
+            ?>
             <button>Signup</button>
         </form>
 
